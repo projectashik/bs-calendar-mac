2f0ffaf2f174016f14948c1c8cbde614cb79c3b2
Ashik Chapagain <projectashik@gmail.com>
2026-01-11 07:41:32 +0545

    Initial Commit

---
+++ bs-calendar/bs_calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  bs_calendarApp.swift
+//  bs-calendar
+//
+//  Created by Ashik Chapagain on 11/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct bs_calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
